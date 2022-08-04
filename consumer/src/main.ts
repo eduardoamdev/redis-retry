c35bf000f5393179730f48d4a60d19c56afe01f6
eduardoamdev <eduardoamdev@gmail.com>
2022-08-04 14:53:07 +0200

    consumer entry point modified

--- consumer/src/main.ts
+++ consumer/src/main.ts
@@ -2,9 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  //const app = await NestFactory.create(AppModule);
-  const app = await NestFactory.createApplicationContext(AppModule);
-
-  //await app.listen(8013);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
 bootstrap();
